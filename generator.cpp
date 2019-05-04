d7acbd640877087dda68452397f9cd7897949074
Pascal Pons <pascal.pons@gmail.com>
2019-05-04 14:22:15 +0200

    Make generator more robust to invalid input lines

--- generator.cpp
+++ generator.cpp
@@ -2,6 +2,7 @@
 #include "OpeningBook.hpp"
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <unordered_set>
 
@@ -15,8 +16,8 @@ std::unordered_set<uint64_t> visited;
  */
 void explore(const Position &P, char* pos_str, const int depth) {
   uint64_t key = P.key3();
-  if(visited.count(key)) return;  // already explored position
-  visited.insert(key);            // flag new position as visited
+  if(!visited.insert(key).second)
+    return; // already explored position
 
   int nb_moves = P.nbMoves();
   if(nb_moves <= depth)
@@ -35,34 +36,42 @@ void explore(const Position &P, char* pos_str, const int depth) {
 
 /**
  * Read scored positions from stdin and store them in an opening book
+ *
+ * Input lines must be a valid position (possibly empty string), a space and a valid score
+ * Read input until EOF or an empty line is reached.
  */
 void generate_opening_book() {
   static constexpr int BOOK_SIZE = 23; // store 2^BOOK_SIZE positions in the book
-  static constexpr int DEPTH = 14;     // max depth of evry position to be stored
+  static constexpr int DEPTH = 14;     // max depth of every position to be stored
   static constexpr double LOG_3 = 1.58496250072; // log2(3)
   TranspositionTable<uint_t<int((DEPTH + Position::WIDTH -1) * LOG_3) + 1 - BOOK_SIZE>, Position::position_t, uint8_t, BOOK_SIZE> *table =
     new TranspositionTable<uint_t<int((DEPTH + Position::WIDTH -1) * LOG_3) + 1 - BOOK_SIZE>, Position::position_t, uint8_t, BOOK_SIZE>();
 
-  std::string pos;
-  int score;
-  while(getline(std::cin, pos, ' ')) {
-    std::cin >> score;
-    static long long count = 0;
-    if(++count % 1000000 == 0)
-      std::cout << count << std::endl;
+  long long count = 1;
+  for(std::string line; getline(std::cin, line); count++) {
+    if(line.length() == 0) break; // empty line = end of input
+    std::istringstream iss(line);
+    std::string pos;
+    getline(iss, pos, ' '); // read position before first space character
+    int score;
+    iss >> score;
+
     Position P;
-    P.play(pos);
+    if(iss.fail() || !iss.eof()
+        || P.play(pos) != pos.length()
+        || score < Position::MIN_SCORE || score > Position::MAX_SCORE) {  // a valid line is a position a space and a valid score
+      std::cerr << "Invalid line (line ignored): " << line << std::endl;
+      continue;
+    }
     table->put(P.key3(), score - Position::MIN_SCORE + 1);
-    getline(std::cin, pos);
+    if(count % 1000000 == 0) std::cerr << count << std::endl;
   }
 
- OpeningBook book{
-    Position::WIDTH,
-    Position::HEIGHT,
-    DEPTH,
-    table
-  };
-  book.save("7x6.book");
+  OpeningBook book{Position::WIDTH, Position::HEIGHT, DEPTH, table};
+
+  std::ostringstream book_file;
+  book_file << Position::WIDTH << "x" << Position::HEIGHT << ".book";
+  book.save(book_file.str());
 }
 
 /**
